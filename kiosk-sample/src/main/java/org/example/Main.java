2a2630f16af4f0cbefe5dcd6dd229473c01660dc
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-07 01:50:51 +0900

    1차 기능구현
    장바구니에 여러 메뉴가 담길 수 있도록 초기화 하지 않고 값을 유지하기

--- kiosk-sample/src/main/java/org/example/Main.java
+++ kiosk-sample/src/main/java/org/example/Main.java
@@ -32,88 +32,82 @@ public class Main {
         String orderFrozenCustard;
         String orderBurger;
 
-        // 반복되는 부분을 줄이고 싶다..
-        if (customerMenu.equals("Burgers")) {
-            kioskService.burgerMenu();
-            // 상품 메뉴 - 카데고리 메뉴판
-            orderBurger = sc.nextLine();
-            selectBurger(kioskService, orderBurger, sc);
-        }
-        if (customerMenu.equals("Frozen Custard")) {
-            kioskService.frozenCustardMenu();
-            orderFrozenCustard = sc.nextLine();
-            selectFrozenCustard(kioskService, orderFrozenCustard, sc);
-
-        }
-        if (customerMenu.equals("Drinks")) {
-            kioskService.drinksMenu();
-            orderDrinks = sc.nextLine();
-            selectDrinks(kioskService, orderDrinks, sc);
-
-        }
-        if (customerMenu.equals("Beer")) {
-            kioskService.beerMenu();
-            orderBeer = sc.nextLine();
-            selectBeer(kioskService, orderBeer, sc);
-
-        }
-        // [ ORDER MENU ]
-        // 5. Order
-        if (customerMenu.equals("Order")) {
-            kioskService.orderCheck1();  // 아래와 같이 주문 하시겠습니까  Orders
-            // 장바구니 보여주기
-            //  Cart에 추가된 Order 리스트 가져와보기
-            List<Order> orderList = cart.getOrderList();
-            for (Order order : orderList) {
-                System.out.println(
-                    String.format("%s | W %.1f | %s \n",
-                        order.getName(),
-                        order.getPrice(),
-                        order.getDescription()
-                    )
-                );
-            }
-
-            kioskService.orderCheck2();  // Total
-            double totalPrice = cart.calculateTotalprice();
-            System.out.println(" W " + totalPrice);
-            kioskService.orderCheck3();   // 주문, 메뉴판
-
-            String completeOrder = sc.nextLine();
-            if (completeOrder.equals("1. 주문")) {
-                kioskService.decideOrder1();
-
-                // 대기번호 주어야 한다.
-                int waitingNumber = 0;
-                waitingNumber += 1;
-                System.out.println("대기번호는 [ " + waitingNumber +" ] 번 입니다.");
-                kioskService.decideOrder2();
-                // 주문이 완료되었으니 장바구니 초기화하기
-                cart.cancelOrder();
+        switch (customerMenu) {
+            case "Burgers":
+                kioskService.burgerMenu();
+                // 상품 메뉴 - 카데고리 메뉴판
+                orderBurger = sc.nextLine();
+                selectBurger(kioskService, orderBurger, sc, cart);
+                break;
+            case "Frozen Custard":
+                kioskService.frozenCustardMenu();
+                orderFrozenCustard = sc.nextLine();
+                selectFrozenCustard(kioskService, orderFrozenCustard, sc, cart);
+                break;
+            case "Drinks":
+                kioskService.drinksMenu();
+                orderDrinks = sc.nextLine();
+                selectDrinks(kioskService, orderDrinks, sc, cart);
+                break;
+            case "Beer":
+                kioskService.beerMenu();
+                orderBeer = sc.nextLine();
+                selectBeer(kioskService, orderBeer, sc, cart);
+                break;
+            case "Order":
+                kioskService.orderCheck1();
+                // 장바구니 보여주기
+                //  Cart에 추가된 Order 리스트 가져와보기
+                List<Order> orderList = cart.getOrderList();
+                for (Order order : orderList) {
+                    System.out.println(
+                        String.format("%s | W %.1f | %s \n",
+                            order.getName(),
+                            order.getPrice(),
+                            order.getDescription()
+                        )
+                    );
+                }
+                kioskService.orderCheck2();  // [Total]
+                double totalPrice = cart.calculateTotalprice();
+                System.out.println(" W " + totalPrice);
+                kioskService.orderCheck3();   // "1.주문, 2.메뉴판"
+                String completeOrder = sc.nextLine();
+                if (completeOrder.equals("1. 주문")) {
+                    kioskService.decideOrder1();
+                    // 대기번호 주어야 한다.
+                    int waitingNumber = 0;
+                    waitingNumber += 1;
+                    System.out.println("대기번호는 [ " + waitingNumber + " ] 번 입니다.");
+                    kioskService.decideOrder2();
+                    // 주문이 완료되었으니 장바구니 초기화하기
+                    cart.cancelOrder();
+                    kioskService.mainMenu();
+                } else if (completeOrder.equals("2. 메뉴판")) {  // 장바구니 초기화 하면 안됨.
+                    kioskService.mainMenu();
+                    orderProcess(sc, kioskService, cart);
+                }
+                break;
+            case "Cancel":
+                kioskService.orderCancel();   // "진행하던 주문을 취소하시겠습니까?\n" +
+                String cancelprogress = sc.nextLine();
+                if (cancelprogress.equals("1. 확인")) {
+                    kioskService.cancelComplete();
+                    cart.cancelOrder();
+                    kioskService.mainMenu();
+                } else if (cancelprogress.equals("2. 취소")) {
+                    kioskService.mainMenu();
+                    orderProcess(sc, kioskService, cart);  // 취소하고 다시 선택할 수 있었으면 좋겠는데 안됨.
+                }
+                break;
+            default:
+                System.out.println("잘못된 입력입니다. 다시 입력해주세요");
                 kioskService.mainMenu();
-            } else if (completeOrder.equals("2. 메뉴판")) {
-                kioskService.mainMenu();
-            }
+                orderProcess(sc, kioskService, cart);
+                break;
         }
-        // 6. Cancel
-         if (customerMenu.equals("Cancel")) {
-            kioskService.orderCancel();   // "진행하던 주문을 취소하시겠습니까?\n" +
-            String cancelprogress = sc.nextLine();
-            if (cancelprogress.equals("1. 확인")) {
-                kioskService.cancelComplete();
-                kioskService.mainMenu();
-                // 장바구니 비우기
-                List<Order> orderList = new ArrayList<>(); // 빈 Order 목록 생성
-                cart = new Cart(orderList); // Cart 객체 생성 시, 위에서 생성한 orderList를 전달
-                cart.cancelOrder();
-            } else if (cancelprogress.equals("2. 취소")) {
-                kioskService.mainMenu();
-            }
-        }
+
     }
-
-
-
 
 
     //메서드
@@ -122,7 +116,7 @@ public class Main {
     data.Drinks[] drinks = data.Drinks.values();
     data.Beers[] beers = Beers.values();
 
-    private static void selectBurger(KioskService kioskService, String orderBurger, Scanner sc) {
+    private static void selectBurger(KioskService kioskService, String orderBurger, Scanner sc, Cart cart) {
         // 방법 1
 //        if (orderBurger.equals(Burgers.BURGER_1.getName())) {
 //            System.out.println(
@@ -163,18 +157,14 @@ public class Main {
             System.out.println(selectedBurger.name + "가 장바구니에 추가되었습니다.\n");
             // 장바구니에 추가하기
             Order orderInfo = new Order(selectedBurger.name, selectedBurger.price, selectedBurger.description, 1);
-            List<Order> orderList = new ArrayList<>();
-            Cart cart = new Cart(orderList);
-            cart.addOrder(orderInfo);
-            kioskService.mainMenu();
-            orderProcess(sc, kioskService, cart);
+            addToCart(orderInfo, cart, sc, kioskService);
             return;  // 출력문이 안나오고 입력가능한 화면이 나왔다가 return 으로 메서드 종료 시켜 출력문이 나오게 되었다.
         } else if (askOrderToCart.equals("2. 취소")) {
             kioskService.mainMenu();
         }
     }
 
-    private static void selectFrozenCustard(KioskService kioskService, String orderFrozenCustrad, Scanner sc) {
+    private static void selectFrozenCustard(KioskService kioskService, String orderFrozenCustrad, Scanner sc, Cart cart) {
         FrozenCustards selectedFrozenCustards = null;
         for (FrozenCustards frozenCustards : FrozenCustards.values()) {
             if (orderFrozenCustrad.equals(frozenCustards.name)) {
@@ -194,18 +184,13 @@ public class Main {
             System.out.println(selectedFrozenCustards.name + "가 장바구니에 추가되었습니다.\n");
             // 장바구니에 추가하기
             Order orderInfo = new Order(selectedFrozenCustards.name, selectedFrozenCustards.price, selectedFrozenCustards.description, 1);
-            List<Order> orderList = new ArrayList<>();
-            Cart cart = new Cart(orderList);
-            cart.addOrder(orderInfo);
-            kioskService.mainMenu();
-            orderProcess(sc, kioskService, cart);
+            addToCart(orderInfo, cart, sc, kioskService);
             return;
         } else if (askOrderToCart.equals("2. 취소")) {
             kioskService.mainMenu();
         }
     }
-
-    private static void selectDrinks(KioskService kioskService, String orderDrinks, Scanner sc) {
+    private static void selectDrinks(KioskService kioskService, String orderDrinks, Scanner sc, Cart cart) {
         Drinks selectedDrinks = null;
         for (Drinks drink : Drinks.values()) {
             if (orderDrinks.equals(drink.name)) {
@@ -225,18 +210,14 @@ public class Main {
             System.out.println(selectedDrinks.name + "가 장바구니에 추가되었습니다.\n");
             // 장바구니에 추가하기
             Order orderInfo = new Order(selectedDrinks.name, selectedDrinks.price, selectedDrinks.description, 1);
-            List<Order> orderList = new ArrayList<>();
-            Cart cart = new Cart(orderList);
-            cart.addOrder(orderInfo);
-            kioskService.mainMenu();
-            orderProcess(sc, kioskService, cart);
+            addToCart(orderInfo, cart, sc, kioskService);
             return;
         } else if (askOrderToCart.equals("2. 취소")) {
             kioskService.mainMenu();
         }
     }
 
-    private static void selectBeer(KioskService kioskService, String orderBeer, Scanner sc) {
+    private static void selectBeer(KioskService kioskService, String orderBeer, Scanner sc, Cart cart) {
         Beers selectedBeer = null;
         for (Beers beer : Beers.values()) {
             if (orderBeer.equals(beer.name)) {
@@ -256,18 +237,18 @@ public class Main {
             System.out.println(selectedBeer.name + "가 장바구니에 추가되었습니다.\n");
             // 장바구니에 추가하기
             Order orderInfo = new Order(selectedBeer.name, selectedBeer.price, selectedBeer.description, 1);
-            List<Order> orderList = new ArrayList<>();
-            Cart cart = new Cart(orderList);
-            cart.addOrder(orderInfo);
-            kioskService.mainMenu();
-            orderProcess(sc, kioskService, cart);
+            addToCart(orderInfo, cart, sc, kioskService);
             return;
         } else if (askOrderToCart.equals("2. 취소")) {
             kioskService.mainMenu();
         }
 
     }
-
+    private static void addToCart(Order selectedOrder, Cart cart, Scanner sc, KioskService kioskService) {
+        cart.addOrder(selectedOrder);
+        kioskService.mainMenu();
+        orderProcess(sc, kioskService, cart);
+    }
 
 }
 
